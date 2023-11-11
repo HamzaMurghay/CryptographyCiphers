c74bf0c27507ec91ad05363d01a6d57ba8859e27
Hamza <hamzamurghay@gmail.com>
2023-11-11 14:27:11 +0530

    added feature to return encrypted/decrypted result in the exact same type case as inputted, not in lower case as was previously

--- Shift Cipher (AKA Ceasar Cipher).py
+++ Shift Cipher (AKA Ceasar Cipher).py
@@ -1,7 +1,10 @@
 import random
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-           'w', 'x', 'y', 'z']
+small_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                 'u', 'v', 'w', 'x', 'y', 'z']
+
+capital_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                   'U', 'V', 'W', 'X', 'Y', 'Z']
 
 key = random.randint(1, 25)
 
@@ -31,30 +34,26 @@ decrypted_output = ''
 
 mode = input("Which mode would you like to use?(Encrypt/Decrypt Mode): ").lower()
 
-run = True
+while True:
 
-while run:
-
-    if mode == "exit":
-        run = False
-        continue
+    if mode == "exit": break
 
     if mode == 'e':
-        to_encrypt = input("Enter Sentence/Word to Encrypt: ").lower()
+        to_encrypt = input("Enter Sentence/Word to Encrypt: ")
 
-        if to_encrypt == "exit":
-            run = False
-            continue
+        if to_encrypt.lower() == "exit": break
 
         for letter in to_encrypt:
             if letter == ' ':
                 encrypted_output += letter
-            elif letter in letters:
-                encrypted_output += letters[(letters.index(letter) + key) % 26]
+            elif letter in small_letters:
+                encrypted_output += small_letters[(small_letters.index(letter) + key) % 26]
+            elif letter in capital_letters:
+                encrypted_output += capital_letters[(capital_letters.index(letter) + key) % 26]
             else:
                 continue
 
-        print("ENCRYPTED OUTPUT:", encrypted_output.lower(), "\n\n")
+        print("ENCRYPTED OUTPUT:", encrypted_output, "\n\n")
         encrypted_output = ''
         mode = input("Which mode would you like to use?(Encrypt/Decrypt Mode): ").lower()
 
@@ -63,27 +62,26 @@ while run:
 
         if (not d_key.isdigit()) or int(d_key) > 25 or int(d_key) <= 0:
             if d_key == "exit":
-                run = False
-                continue
+                break
             else:
                 print("Enter a valid key!\n\n")
                 continue
 
-        to_decrypt = input("Enter Sentence/Word to Decrypt: ").lower()
+        to_decrypt = input("Enter Sentence/Word to Decrypt: ")
 
-        if to_decrypt == "exit":
-            run = False
-            continue
+        if to_decrypt == "exit": break
 
         for letter in to_decrypt:
             if letter == ' ':
                 decrypted_output += letter
-            elif letter in letters:
-                decrypted_output += letters[(letters.index(letter) - key) % 26]
+            elif letter in small_letters:
+                decrypted_output += small_letters[(small_letters.index(letter) - key) % 26]
+            elif letter in capital_letters:
+                decrypted_output += capital_letters[(capital_letters.index(letter) - key) % 26]
             else:
                 continue
 
-        print("DECRYPTED OUTPUT:", decrypted_output.lower(), "\n\n")
+        print("DECRYPTED OUTPUT:", decrypted_output, "\n\n")
         decrypted_output = ''
         mode = input("Which mode would you like to use?(Encrypt/Decrypt Mode): ").lower()
 
